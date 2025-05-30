751bd046cb33a71aefc872e67a7f96228137fbc1
Eli Moretti <144716146+EliMorettiCS@users.noreply.github.com>
2025-05-30 10:10:29 -0700

    Calendar System First Attempt
    Tried to build a basic calendar function. Still untested and will be tested and refined in future updates.

--- Scripts/Calender.gd
+++ Scripts/Calender.gd
@@ -1,8 +1,42 @@
-extends Node
-var day = 0
-var month = 0
-var year = 0
-
-func _init():
-	print("Constructed!")
+@tool
+extends EditorScript
+var day
+var month
+var year
+var dayspassed
 	
+func _init():
+	day = 30
+	month = 5
+	year = 2025
+	dayspassed = 0
+
+func printdate():
+	print(str(month)+"/"+str(day)+"/"+str(year).substr(2,3))
+	
+func _run():
+	printdate()
+	
+func nextday():
+	dayspassed+=1
+	if month == 1 or 2 or 4 or 6 or 7 or 9 or 11:
+		if day >= 31:
+			day = 0
+			month += 1
+		else:
+			day += 1
+	elif month == 3 or 5 or 10 or 12:
+		if day >= 30:
+			day = 0
+			month += 1
+		else:
+			day += 1
+	elif month == 8:
+		if day >= 28:
+			day = 0
+			month += 1
+		else:
+			day += 1
+	if month >= 12:
+		month = 0
+		year += 1

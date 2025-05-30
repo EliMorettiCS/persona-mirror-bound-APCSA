0afd322e2a6327014a35991cc1d2052cf8cc82e9
Eli Moretti <144716146+EliMorettiCS@users.noreply.github.com>
2025-05-30 10:44:20 -0700

    Calendar Tweaks
    The Calendar SHOULD work properly now. hopefully. May still need tweaks idk but close enough for now.

--- Scripts/Calender.gd
+++ Scripts/Calender.gd
@@ -1,42 +1,45 @@
 @tool
 extends EditorScript
+
 var day
 var month
 var year
 var dayspassed
 	
 func _init():
-	day = 30
-	month = 5
+	day = 1
+	month = 1
 	year = 2025
 	dayspassed = 0
 
 func printdate():
 	print(str(month)+"/"+str(day)+"/"+str(year).substr(2,3))
 	
-func _run():
-	printdate()
-	
 func nextday():
 	dayspassed+=1
-	if month == 1 or 2 or 4 or 6 or 7 or 9 or 11:
+	if month == 1 or month == 2 or month == 4 or month == 6 or month == 7 or 9 or month == 11:
 		if day >= 31:
-			day = 0
+			day = 1
 			month += 1
 		else:
 			day += 1
-	elif month == 3 or 5 or 10 or 12:
+	elif month == month == 3 or month == 5 or month == 10 or month == 12:
 		if day >= 30:
-			day = 0
+			day = 1
 			month += 1
 		else:
 			day += 1
 	elif month == 8:
 		if day >= 28:
-			day = 0
+			day = 1
 			month += 1
 		else:
 			day += 1
 	if month >= 12:
 		month = 0
 		year += 1
+		
+func _run():
+	for i in range(100):
+		printdate()
+		nextday()
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,1 +1,12 @@
-extends Node
+@tool
+extends EditorScript
+
+
+var test
+	
+func _init():
+	test = 1
+
+	
+func _run():
+	print(test)
